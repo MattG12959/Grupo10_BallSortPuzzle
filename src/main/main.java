0dc07a2bb25070b5e57078d2db79ee66a5f6d7e3
Matias Antonacci <matiasgantonacci@gmail.com>
2026-03-26 19:53:44 -0300

    Update main.java

--- src/main/main.java
+++ src/main/main.java
@@ -13,7 +13,7 @@ package main;
 public class main {
 
     public static void main(String[] args) {
-        System.out.println("Prueba");
+        System.out.println("Prueba 2");
     }    
     
 }
